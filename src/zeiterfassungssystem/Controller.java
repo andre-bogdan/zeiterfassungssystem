4c444108a02de4d40856725795a57797cbf99b2d
Andre Bogdan <abogdan@web.de>
2019-01-28 12:55:22 +0100

    20190128

--- src/zeiterfassungssystem/Controller.java
+++ src/zeiterfassungssystem/Controller.java
@@ -1,23 +1,38 @@
 package zeiterfassungssystem;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+
+import static javafx.scene.input.KeyCode.O;
 
 public class Controller {
     //Variablen
     @FXML
-    Pane rootPane, mitAnlegen, mitBearbeiten, mitLoeschen;
-
+    Pane rootPane, einstellungen, mitAnlegen, mitBearbeiten, mitLoeschen;
     @FXML
-    Label messageAnlegen, messageBearbeiten, messageLoeschen;
+    Label messageEinstellungen, messageAnlegen, messageBearbeiten, messageLoeschen;
+    @FXML
+    ComboBox bundeslaender;
+
+    ObservableList<String> laender = FXCollections.observableArrayList("Baden-Württemberg","Bayern","Berlin","Brandenburg","Bremen","Hamburg","Hessen","Mecklenburg-Vorpommern","Niedersachsen","Nordrhein-Westfalen","Rheinland-Pfalz","Saarland,Sachsen","Sachsen-Anhalt","Schleswig-Holstein","Thüringen");
 
     //Initialisierung der Oberflaeche
     public void initialize(){
         rootPane.getChildren().clear();
     }
 
-    //Programm beenden
+    //Datei
+    public void einstellungen(){
+        rootPane.getChildren().clear();
+        rootPane.getChildren().add(einstellungen);
+        bundeslaender.setItems(laender);
+        messageEinstellungen.setText("test");
+    }
+
     public void progBeenden(){
         System.exit(0);
     }
